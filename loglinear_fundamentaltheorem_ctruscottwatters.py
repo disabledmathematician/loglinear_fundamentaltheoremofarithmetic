5e790dc34d0e319ba9228ae9cd11ae35d1adb8cc
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2026-04-24 22:20:21 +1000

    Add files via upload

--- loglinear_fundamentaltheorem_ctruscottwatters.py
+++ loglinear_fundamentaltheorem_ctruscottwatters.py
@@ -1,22 +1,28 @@
+import math
 def bisection_factor(n):
 	factors = []
-	for x in range(1, n):
+	for x in range(0, n):
 		high = n
 		low = 0
-		guess = (high + low) // 2
-		while((guess * x)  -  n > 0.0000001):
-			print(high, low, guess)
+		guess = (high + low) / 2.0
+		while (((int(guess) * x) - n) > 0.000001):
+#			print(guess, x)
 			if ((guess * x) > n):
 				high = guess
 			if ((guess * x) < n):
 				low = guess
-			guess = (high + low) // 2
-		if guess * x == n:
-			factors.append((guess, x))
+			guess = (high + low) / 2.0
+			if (int(guess) * x) == n:
+				factors.append((int(guess), x))
+		high, low, guess = 0, 0, 0
 	return factors
 	
 def Charles_FundamentalTheorem():
-	n = 1992
-	print(bisection_factor(n))
+	for n in range(2, 100):
+		factors = bisection_factor(n)
+		print(f"Factors of {n} are {factors}")
+#	
+#	n = 2481
+#	print(bisection_factor(n))
 	
 Charles_FundamentalTheorem()
